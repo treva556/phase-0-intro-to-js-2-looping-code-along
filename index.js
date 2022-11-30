ded39bb47d9db079646f6497fb74898cfcea2fc9
treva556 <marktreva92@gmail.com>
2022-11-30 14:08:06 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+function writeCards(name, event) {
+    let nameToGreet = [];
+    for (let i = 0; i < name.length; i++) {
+      nameToGreet.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+    }
+    return nameToGreet;
+  }
+  
+  function countDown(num) {
+    //   let number = num;
+    while (0 <= num) {
+      console.log(num--);
+    }
+  }
+  countDown(10);
